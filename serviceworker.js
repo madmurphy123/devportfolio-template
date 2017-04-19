5b56753b4a30e0fcadf280363f67cf24d28a1520
madmurphy123 <murphyaaron123@gmail.com>
2017-04-19 15:41:36 +0100

    push

--- serviceworker.js
+++ serviceworker.js
@@ -57,7 +57,7 @@ var CACHED_URLS = [
   // CSS and fonts
     'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'css/bootstrap.css',
+    'css/bootstrap.min.css',
     'css/styles.css'
 ];
 
