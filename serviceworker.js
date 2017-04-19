5ae9fc64f7d0b4411dbaa271d6b1732aef0480fc
madmurphy123 <murphyaaron123@gmail.com>
2017-04-19 15:39:36 +0100

    push

--- serviceworker.js
+++ serviceworker.js
@@ -47,8 +47,7 @@ var CACHED_URLS = [
 
     // JavaScript
     BASE_PATH + 'offline-map.js',
-    BASE_PATH + 'material.js',
-    BASE_PATH + 'scripts.js',
+    BASE_PATH + 'js/scripts.js',
     
     //json
     BASE_PATH + 'events.json',
@@ -58,8 +57,8 @@ var CACHED_URLS = [
   // CSS and fonts
     'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    BASE_PATH + 'min-style.css',
-    BASE_PATH + 'styles.css'
+    BASE_PATH + 'css/bootstrap.css',
+    BASE_PATH + 'css/styles.css'
 ];
 
 var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyD8GS9IEYRrTEbXtN7rI1Z6in3XFB9z2W0&callback=initMap';
