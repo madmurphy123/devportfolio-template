6adbfce89609265b7f0f9f5ba933266d3bc4d7d8
madmurphy123 <murphyaaron123@gmail.com>
2017-04-27 13:58:08 +0100

    new

--- js/news.js
+++ js/news.js
@@ -5,19 +5,20 @@ if(newsContainer){
             return response.json();
         }).then(news => {
             const newsHTML = news.articles.map(article => {
-                return `<div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
-                    <div class="mdl-card__media">
-                        <img class="article-image" src="${article.urlToImage}" border="0" alt="">
+                return `
+                  <div class="project shadow-large">
+                    <div class="blog-image">
+                        <img src="${article.urlToImage}" />
                     </div>
-                    <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">${article.title}</h2>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>${article.title}</h3>
+                        <p>
+                            ${article.description}
+                        </p>
+                        <a href="" target="blank">Read More</a>
                     </div>
-                    <div class="mdl-card__supporting-text">
-                        ${article.description}
-                    </div>
-                    <div class="mdl-card__actions mdl-card--border">
-                        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="${article.url}" data-upgraded=",MaterialButton,MaterialRipple">Read more<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a>
-                    </div>
+                    <!-- End .project-info -->
                 </div>`;
             }).join("\n");
             
