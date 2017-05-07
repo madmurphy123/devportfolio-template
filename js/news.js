c48e9e9d851e8c20a9ce81a5640503aeaa191dc6
madmurphy123 <murphyaaron123@gmail.com>
2017-05-07 16:38:28 +0100

    new

--- js/news.js
+++ js/news.js
@@ -1,58 +1,58 @@
-// const newsContainer = document.getElementById('news');
-// if(newsContainer){
-//     fetch("https://newsapi.org/v1/articles?source=bbc-news&apiKey=a5ba2a0461c24459b8c4f3e746c9ae8f")
-//         .then(response => {
-//             return response.json();
-//         }).then(news => {
-//             const newsHTML = news.articles.map(article => {
-//                 return `
-//                   <div class="project shadow-large">
-//                     <div class="blog-image">
-//                         <img src="${article.urlToImage}" alt="${article.title}" />
-//                     </div>
-//                     <!-- End .project-image -->
-//                     <div class="project-info">
-//                         <h3>${article.title}</h3>
-//                         <p>
-//                             ${article.description}
-//                         </p>
-//                         <a href=" ${article.url}" target="blank">Read More</a>
-//                     </div>
-//                     <!-- End .project-info -->
-//                 </div>`;
-//             }).join("\n");
-            
-//             newsContainer.innerHTML = newsHTML;
-//         });
-// }
-
-
-const eventsContainer = document.getElementById('events');
-if(eventsContainer){
-    fetch("events.json")
+const newsContainer = document.getElementById('news');
+if(newsContainer){
+    fetch("https://newsapi.org/v1/articles?source=bbc-news&apiKey=a5ba2a0461c24459b8c4f3e746c9ae8f")
         .then(response => {
             return response.json();
-        }).then(events => {
-            const eventsHTML = events.map(event => {
-                return `<div class="project shadow-large">
+        }).then(news => {
+            const newsHTML = news.articles.map(article => {
+                return `
+                  <div class="project shadow-large">
                     <div class="blog-image">
-                        <img src="${event.picture}" alt="${event.heading}" />
+                        <img src="${article.urlToImage}" alt="${article.title}" />
                     </div>
                     <!-- End .project-image -->
                     <div class="project-info">
-                        <h3>${event.heading}</h3>
+                        <h3>${article.title}</h3>
                         <p>
-                            ${event.text}
+                            ${article.description}
                         </p>
-                        <a href=" ${event.link}" target="blank">Read More</a>
+                        <a href=" ${article.url}" target="blank">Read More</a>
                     </div>
                     <!-- End .project-info -->
                 </div>`;
-              }).join("\n");
+            }).join("\n");
             
-            eventsContainer.innerHTML = eventsHTML;
+            newsContainer.innerHTML = newsHTML;
         });
 }
+
+
+// const eventsContainer = document.getElementById('events');
+// if(eventsContainer){
+//     fetch("events.json")
+//         .then(response => {
+//             return response.json();
+//         }).then(events => {
+//             const eventsHTML = events.map(event => {
+//                 return `<div class="project shadow-large">
+//                     <div class="blog-image">
+//                         <img src="${event.picture}" alt="${event.heading}" />
+//                     </div>
+//                     <!-- End .project-image -->
+//                     <div class="project-info">
+//                         <h3>${event.heading}</h3>
+//                         <p>
+//                             ${event.text}
+//                         </p>
+//                         <a href=" ${event.link}" target="blank">Read More</a>
+//                     </div>
+//                     <!-- End .project-info -->
+//                 </div>`;
+//               }).join("\n");
+            
+//             eventsContainer.innerHTML = eventsHTML;
+//         });
+// }
 
 
 
