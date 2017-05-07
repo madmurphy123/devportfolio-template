8935572a6c3a2eef1db1a90f41fcac96fa1de7ee
madmurphy123 <murphyaaron123@gmail.com>
2017-05-07 16:15:11 +0100

    new

--- js/news.js
+++ js/news.js
@@ -34,24 +34,43 @@ if(eventsContainer){
             return response.json();
         }).then(events => {
             const eventsHTML = events.map(event => {
-                return `<div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
-                    <div class="mdl-card__media">
-                        <img class="article-image" src="${event.picture}" border="0" alt="">
+                return `
+
+                 <div class="project shadow-large">
+                    <div class="blog-image">
+                        <img src="${event.picture}" alt="${event.heading}" />
                     </div>
-                    <div class="mdl-card__title">
-                        <h2 class="mdl-card__title-text">${event.heading}</h2>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>${event.heading}</h3>
+                        <p>
+                             ${event.text}
+                         </p>
+                         <a href=" ${article.url}" target="blank">Read More</a>
                     </div>
-                    <div class="mdl-card__supporting-text">
-                        ${event.text}
-                    </div>
-                    <div class="mdl-card__actions mdl-card--border">
-                        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="${event.link}" data-upgraded=",MaterialButton,MaterialRipple">Read more<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a>
-                    </div>
-                </div>`;
+                    <!-- End .project-info -->
+                 </div>`;
             }).join("\n");
             
             eventsContainer.innerHTML = eventsHTML;
         });
 }
 
+
+
+
+// <div class="mdl-cell mdl-card mdl-shadow--4dp portfolio-card">
+//                     <div class="mdl-card__media">
+//                         <img class="article-image" src="${event.picture}" border="0" alt="">
+//                     </div>
+//                     <div class="mdl-card__title">
+//                         <h2 class="mdl-card__title-text">${event.heading}</h2>
+//                     </div>
+//                     <div class="mdl-card__supporting-text">
+//                         ${event.text}
+//                     </div>
+//                     <div class="mdl-card__actions mdl-card--border">
+//                         <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="${event.link}" data-upgraded=",MaterialButton,MaterialRipple">Read more<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span></a>
+//                     </div>
+//                 </div>`;
 
